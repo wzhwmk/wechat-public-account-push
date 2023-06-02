f7d6b0f6260ec980597aa892f702a16d0082130c
wzhwmk <73280398+wzhwmk@users.noreply.github.com>
2023-06-02 16:04:49 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,7 +6,9 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-
+  
+  // 请将以下代码按照图示粘贴到对应位置
+  USE_PASSAGE: 'push-plus',
   // 使用微信测试号：公众号APP_ID
   APP_ID: '',
 
@@ -21,9 +23,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: '2eabf7b855c24014ad8a9f86f4f3bbae',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
